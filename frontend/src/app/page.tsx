156661adc8d8f9175950a1110c9bb9889e0f902a
solji <solji.choi89@gmail.com>
2025-01-28 04:02:34 +0900

    폰트어썸 설치

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faPerson } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
     <div>
       <h1>안녕하세요.</h1>
+      <FontAwesomeIcon
+        icon={faThumbsUp}
+        className="fa-fw text-4xl text-[#af0000] hover:text-[#000000]"
+      />
+      <FontAwesomeIcon icon={faPerson} className="fa-fw text-4xl" />
     </div>
   )
 }
