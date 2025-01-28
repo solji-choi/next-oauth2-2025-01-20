ac57acedd1dad9ac087b72cef05a366540d0e5af
solji <solji.choi89@gmail.com>
2025-01-29 03:30:29 +0900

    DefaultOAuth2UserService 의 구현체를 추가하여 SecurityUser가 생성되도록

---
+++ backend/src/main/java/com/ll/global/security/CustomOAuth2UserService.java
@@ -0,0 +1,44 @@
+package com.ll.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    // 소셜 로그인이 성공할 때마다 이 함수가 실행된다.
+    @Transactional
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String oauthId = oAuth2User.getName();
+        String providerTypeCode = userRequest
+                .getClientRegistration()
+                .getRegistrationId()
+                .toUpperCase(Locale.getDefault());
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        Map<String, String> attributesProperties = (Map<String, String>) attributes.get("properties");
+
+        String nickname = attributesProperties.get("nickname");
+        String profileImgUrl = attributesProperties.get("profile_image");
+        String username = providerTypeCode + "__" + oauthId;
+
+        return new SecurityUser(
+                0,
+                username,
+                "",
+                nickname,
+                List.of()
+        );
+    }
+}
--- backend/src/main/java/com/ll/global/security/SecurityUser.java
+++ backend/src/main/java/com/ll/global/security/SecurityUser.java
@@ -3,10 +3,12 @@ package com.ll.global.security;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 
-public class SecurityUser extends User {
+public class SecurityUser extends User implements OAuth2User {
     @Getter
     private long id;
     @Getter
@@ -22,5 +24,15 @@ public class SecurityUser extends User {
         super(username, password, authorities);
         this.id = id;
         this.nickname = nickname;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of();
+    }
+
+    @Override
+    public String getName() {
+        return getUsername();
     }
 }
