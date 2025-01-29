ff59b361094e1b894cd71201ca9fe99c85ef8be5
solji <solji.choi89@gmail.com>
2025-01-29 19:46:53 +0900

    nextConfig 에서 domains(deprecated) 대신 remotePatterns 사용

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    dangerouslyAllowSVG: true, // svg 이미지도 허용하도록 설정
-    domains: ["k.kakaocdn.net", "img1.kakaocdn.net", "placehold.co"],
-    contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https:;", // 보안설정
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "http",
+        hostname: "img1.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "img1.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+    contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https:;",
   },
 };
 
