4d205b918c50f53d2a70426c149f05b44d7a356a
solji <solji.choi89@gmail.com>
2025-01-29 16:15:52 +0900

    세션끄기

--- backend/src/main/java/com/ll/global/security/SecurityConfig.java
+++ backend/src/main/java/com/ll/global/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -59,6 +61,13 @@ public class SecurityConfig {
                                                 authorizationEndpoint
                                                         .authorizationRequestResolver(customAuthorizationRequestResolver)
                                 )
+                )
+                .formLogin(
+                        AbstractHttpConfigurer::disable
+                )
+                .sessionManagement(
+                        sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(
